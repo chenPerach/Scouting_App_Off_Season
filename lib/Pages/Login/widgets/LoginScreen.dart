ab771ea04fe3218e69393f628c59bb51aa6347bc
chenPearch <45401724+chenPearch@users.noreply.github.com>
2021-05-16 17:20:05 +0300

    🔒 added the start of the bloc pattern

--- lib/Pages/Login/widgets/LoginScreen.dart
+++ lib/Pages/Login/widgets/LoginScreen.dart
@@ -1,25 +1,81 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scouting_app_2/Pages/Login/bloc/authentication_bloc.dart';
 import 'package:scouting_app_2/Pages/Login/widgets/login.dart';
 import 'package:scouting_app_2/Pages/Login/widgets/register.dart';
+
 // ignore: camel_case_types
+class BlocHandler extends StatelessWidget {
+  final PageController controller = PageController(initialPage: 0);
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => AuthenticationBloc(),
+    
+      child: ChangePage(controller: controller),
+    );
+  }
+}
+
 class ChangePage extends StatelessWidget {
-  //const ChangePage({Key? key}) : super(key: key);
+  const ChangePage({
+    Key key,
+    @required this.controller,
+  }) : super(key: key);
+
+  final PageController controller;
 
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: 0);
-    return PageView(
-      scrollDirection: Axis.horizontal,
-      controller: controller,
-      children: <Widget>[
-        Center(
-          child: Login(),
-        ),
-        Center(
-          child: Register(),
-        ),
-      ],
+    return BlocListener<AuthenticationBloc, AuthenticationState>(
+      bloc: context.read<AuthenticationBloc>(),
+      listener: (context, state) {
+        if (state is Authenticated) {
+          print("User Succesfuly Authenticated");
+        }
+      },
+      child: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        bloc: context.read<AuthenticationBloc>(),
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          if (state is AuthError) {
+            return PageView(
+              scrollDirection: Axis.horizontal,
+              controller: controller,
+              children: <Widget>[
+                Center(
+                  child: Login(),
+                ),
+                Center(
+                  child: Register(),
+                ),
+              ],
+            );
+          }
+          if(state is AuthenticationInitial){
+            return PageView(
+              scrollDirection: Axis.horizontal,
+              controller: controller,
+              children: <Widget>[
+                Center(
+                  child: Login(),
+                ),
+                Center(
+                  child: Register(),
+                ),
+              ],
+            );
+          }
+          return null;
+        },
+      ),
     );
   }
 }
@@ -30,7 +86,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ChangePage(),
+      body: BlocHandler(),
     );
   }
 }
--- lib/Pages/Login/widgets/login.dart
+++ lib/Pages/Login/widgets/login.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scouting_app_2/Pages/Login/bloc/authentication_bloc.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -12,14 +14,14 @@ class LoginState extends State<Login> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
 
-  final pass = TextEditingController();
-  final user = TextEditingController();
+  final password = TextEditingController();
+  final email = TextEditingController();
 
 
   @override
   void dispose() {
-    pass.dispose();
-    user.dispose();
+    password.dispose();
+    email.dispose();
     super.dispose();
   }
 
@@ -28,82 +30,83 @@ class LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Image(
-              image: NetworkImage(
-                  'https://i.pinimg.com/originals/79/3d/c9/793dc92fc5b0e5fc85c9a1e31efa0749.jpg'),
-            ),
-            Padding(
-              padding: EdgeInsets.all(15),
-              child: TextFormField(
-                controller: user,
-                decoration: const InputDecoration(
-                  hintText: 'name',
-                  labelText: 'user name',
-                  icon: Icon(Icons.person),
-                ),
-                validator: (name) {
-                  if (name == null || name.isEmpty) {
-                    return 'please enter your name';
-                  }
-                  return null;
-                },
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Image(
+                image: NetworkImage(
+                    'https://i.pinimg.com/originals/79/3d/c9/793dc92fc5b0e5fc85c9a1e31efa0749.jpg'),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(15),
-              child: TextFormField(
-                controller: pass,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'password',
-                  labelText: 'password',
-                  icon: Icon(Icons.alarm),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: TextFormField(
+                  controller: email,
+                  decoration: const InputDecoration(
+                    hintText: 'email',
+                    labelText: 'enter your email',
+                    icon: Icon(Icons.person),
+                  ),
+                  validator: (name) {
+                    if (name == null || name.isEmpty) {
+                      return 'please enter your name';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (pass) {
-                  if (pass == null || pass.isEmpty) {
-                    return 'empty';
-                  }
-                  return null;
-                },
               ),
-            ),
-            Center(
-              child: Container(
-                height: 60,
-                width: 300,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: FlatButton(
-                      onPressed: () {
-                        // Validate returns true if the form is valid, or false otherwise.
-                        if (_formKey.currentState.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('Processing Data')));
-                        }
-                      },
-                      child: Text(
-                        'log in',
-                        style: TextStyle(color: Colors.orange, fontSize: 25),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: TextFormField(
+                  controller: password,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'password',
+                    labelText: 'password',
+                    icon: Icon(Icons.alarm),
+                  ),
+                  validator: (pass) {
+                    if (pass == null || pass.isEmpty) {
+                      return 'empty';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 60,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: FlatButton(
+                        onPressed: () async {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState.validate()) {
+                            var bloc = BlocProvider.of<AuthenticationBloc>(context);
+                            bloc.add(AuthLogin(email: email.text , password: password.text));
+                          }
+                        },
+                        child: Text(
+                          'log in',
+                          style: TextStyle(color: Colors.orange, fontSize: 25),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
+
 }
