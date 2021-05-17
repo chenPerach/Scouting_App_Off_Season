b53f797b00a5d68030efdacfd7220f16ae02b417
chenPearch <45401724+chenPearch@users.noreply.github.com>
2021-05-17 19:31:59 +0300

    Revert "possible revert"
    This reverts commit ba6ad9b544e47108c03cb97f15fa7a42b669e06e.

--- lib/Pages/Home/Home.dart
+++ lib/Pages/Home/Home.dart
@@ -1,17 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:scouting_app_2/ChangeNotifiers/UserContainer.dart';
 import 'package:scouting_app_2/services/firebase_auth_service.dart';
 
 class Home extends StatelessWidget {
+  static const String route = '/home';
   @override
   Widget build(BuildContext context) {
+    UserContainer uc = Provider.of<UserContainer>(context);
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-            Text(FirebaseAuthService.instance.user.displayName),
-            TextButton(onPressed: () => FirebaseAuthService.instance.signOut(), child: Text("sign out"))
+            Text(uc.user?.firebaseUser?.displayName ?? "Loading..."),
+            TextButton(onPressed: () => FirebaseAuthService.instance.signOut(), child: Text("sign out")),
+            // TextButton(onPressed: () => Navigator.of(context).pushNamed("/"), child: Text("test"))
           ],
         ),
       ),
