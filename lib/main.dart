ba6ad9b544e47108c03cb97f15fa7a42b669e06e
chenPearch <45401724+chenPearch@users.noreply.github.com>
2021-05-17 19:26:21 +0300

    possible revert

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'package:scouting_app_2/ChangeNotifiers/UserContainer.dart';
-import 'package:scouting_app_2/route_manager.dart';
 import 'package:scouting_app_2/services/firebase_auth_service.dart';
 
 import 'Pages/Home/Home.dart';
@@ -19,45 +16,35 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final Future<FirebaseApp> _isInitilazed = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => UserContainer())
-      ],
-      child: MaterialApp(
-        theme: ThemeData.dark(),
-        initialRoute: FirebaseInitilaize.route,
-        onGenerateRoute: RouteGenerator.generateRoute,
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: FutureBuilder(
+        future: _isInitilazed,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done)
+            return HandleAuth();
+          else
+            return _WaitingApp();
+        },
       ),
     );
   }
 }
 
-class FirebaseInitilaize extends StatelessWidget {
-  static const String route = "/";
-  final Future<FirebaseApp> _isInitilazed = Firebase.initializeApp();
-
+class HandleAuth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _isInitilazed,
+    return StreamBuilder(
+      stream: FirebaseAuthService.instance.authState(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done)
-          return StreamBuilder(
-            stream: FirebaseAuthService.instance.authState(),
-            builder: (context, snapshot) {
-              return snapshot.data == null ? LoginScreen() : Home();
-            },
-          );
-        else
-          return _WaitingApp();
+        return snapshot.data == null ? LoginScreen() : Home();
       },
     );
   }
 }
-
-
 
 class _WaitingApp extends StatelessWidget {
   @override
