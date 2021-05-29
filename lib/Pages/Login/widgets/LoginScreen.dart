6988996e0c42084b42041ca6b1560eb212715f12
chen <chen76273@gmail.com>
2021-05-29 12:15:03 +0300

    login page refactoring to new Util class "PageBloc Creator"

--- lib/Pages/Login/widgets/LoginScreen.dart
+++ lib/Pages/Login/widgets/LoginScreen.dart
@@ -5,59 +5,6 @@ import 'package:scouting_app_2/Pages/Login/widgets/login.dart';
 import 'package:scouting_app_2/Pages/Login/widgets/register.dart';
 import 'package:scouting_app_2/Pages/WaitingPage/Waiting.dart';
 import 'package:scouting_app_2/Utils/BlocCreator.dart';
-
-// class ChangePage extends StatelessWidget {
-//   ChangePage({
-//     Key key,
-//     @required this.controller,
-//   }) : super(key: key);
-
-//   final PageController controller;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocListener<AuthenticationBloc, AuthenticationState>(
-//       bloc: context.read<AuthenticationBloc>(),
-//       listener: (context, state) {
-//         if (state is Authenticated) {
-//           print("User Succesfuly Authenticated");
-//         }
-//       },
-//       child: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-//         bloc: context.read<AuthenticationBloc>(),
-//         builder: (context, state) {
-//           if (state is AuthLoading) {
-//             return Waiting();
-//           }
-//           if (state is AuthError) {
-//             var e = state.exception;
-//             return PageView(
-//               scrollDirection: Axis.horizontal,
-//               controller: controller,
-//               children: <Widget>[
-//                 Login(exception: e.happendOn == "LOGIN" ? e : null),
-//                 Register(exception: e.happendOn == "REGISTER" ? e : null),
-//               ],
-//             );
-//           }
-//           if (state is AuthenticationInitial || state is Authenticated) {
-//             return PageView(
-//               scrollDirection: Axis.horizontal,
-//               controller: controller,
-//               children: <Widget>[
-//                 Login(),
-//                 Register(),
-//               ],
-//             );
-//           }
-
-//           return Waiting();
-//         },
-//       ),
-//     );
-//   }
-// }
-
 class LoginScreen extends StatelessWidget {
   static const String route = '/login';
   final PageController controller = PageController(initialPage: 0);
@@ -80,7 +27,7 @@ class LoginScreen extends StatelessWidget {
     }
     if (state is AuthError) {
       var e = state.exception;
-      // controller.jumpToPage(e.happendOn == 'LOGIN'?0:1);
+      
       return PageView(
         scrollDirection: Axis.horizontal,
         controller: controller,
@@ -88,7 +35,7 @@ class LoginScreen extends StatelessWidget {
           Login(exception: e.happendOn == "LOGIN" ? e : null),
           Register(exception: e.happendOn == "REGISTER" ? e : null),
         ],
-      );
+      ) ;
     }
     if (state is AuthenticationInitial || state is Authenticated) {
       return PageView(
